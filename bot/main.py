2891bffb80a5bf99018154a79d0846d1ff5718ef
kevsohn <kevin.sohn39@gmail.com>
2025-08-07 17:13:18 +0100

    got the bot to send an embedded link on command

--- bot/main.py
+++ bot/main.py
@@ -16,8 +16,15 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    #if message.content.lower().startswith("!minesweeper"):
+
     if message.content.lower().startswith('!simon'):
-        await message.reply('Hello! Do you want to play a game? :>', mention_author=True)
+        embed = discord.Embed(title="🎮 Simon Says...",
+                              description="Click the link below to start:",
+                              color=0x00ff00)
+        embed.add_field(name="Game Link", value="[Play Now](https://yourgame.com/play)", inline=False)
+        #await message.channel.send(embed=embed)
+        await message.reply(embed=embed)
 
 with open('token.txt', 'r') as file:
     token = file.readline().strip()
