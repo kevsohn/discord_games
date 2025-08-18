029146c5a45c418b8b8a98728d31b38ce6ffc32f
kevsohn <kevin.sohn39@gmail.com>
2025-08-18 23:56:24 +0100

    added basic postgresql db to server

--- discord_games/server.py
+++ discord_games/server.py
@@ -1,7 +1,12 @@
 from flask import Flask, session, request, render_template, redirect, url_for
 from flask_session import Session
 from secrets import token_urlsafe
+import os
+import psycopg2 as psql
+from psycopg2.extras import RealDictCursor
+
 from games.simon import simon_bp
+
 
 # can only have 1 app instance, which necessitates Blueprints
 # to keep things modular and organized
@@ -12,8 +17,41 @@ app.register_blueprint(simon_bp, url_prefix='/simon')
 app.secret_key = token_urlsafe(16)
 # opts: filesystem, redis, memcached, mongodb, sqlalchemy
 app.config['SESSION_TYPE'] = 'filesystem'
-# sessions group data local to each user
+# groups data per sesh
 Session(app)
+
+# portable way to get relative path
+dname = os.path.dirname(__file__)
+fname = os.path.join(dname, 'config.txt')
+with open(fname, 'r') as file:
+    db_user = file.readline().strip()
+db_host = 'localhost'
+
+conn = psql.connect(
+        dbname='game_db',
+        user=db_user,
+        host=db_host,
+        cursor_factory=RealDictCursor
+)
+
+cursor = conn.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS players (
+    id SERIAL PRIMARY KEY,
+    discord_id TEXT UNIQUE NOT NULL,
+    high_score INT DEFAULT 0
+)
+""")
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS session_info (
+    id SERIAL PRIMARY KEY,
+    player_id INT REFERENCES players(id),
+    score INT NOT NULL,
+    played_at TIMESTAMP DEFAULT NOW()
+ )
+""")
+conn.commit()
+
 
 # arg: URL path the client-side calls to perform said action
 @app.route('/')
@@ -39,6 +77,7 @@ def play(game):
         return render_template('minesweeper.html')
     else:
         return 'Game not found!', 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
